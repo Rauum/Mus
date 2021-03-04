9d337e41a5af32c66d5991b23d20bf0c4e319e07
Romain Vaché <rvache001@iutbayonne.univ-pau.fr>
2021-03-04 15:15:21 +0100

    Création des deux équipes avec les 4 joueurs

---
+++ src/main/java/com/montaury/mus/jeu/carte/equipe/Equipe.java
@@ -0,0 +1,26 @@
+package com.montaury.mus.jeu.carte.equipe;
+
+import com.montaury.mus.jeu.joueur.InterfaceJoueurOrdinateur;
+import com.montaury.mus.jeu.joueur.Joueur;
+
+public class Equipe {
+
+    public static Equipe equipe1(String nomJoueur) {
+
+        return new Equipe("Equipe1", Joueur.humain(nomJoueur), Joueur.ordinateur1());
+    }
+    public static Equipe equipe2(){
+        return new Equipe("Equipe1", Joueur.ordinateur2() , Joueur.ordinateur3());
+    }
+
+    private final String nom;
+    public final Joueur joueur1;
+    public final Joueur joueur2;
+
+
+    public Equipe(String nom, Joueur joueur1, Joueur joueur2){
+        this.nom = nom;
+        this.joueur1 = joueur1;
+        this.joueur2 = joueur2;
+    }
+}
